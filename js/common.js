bbb834af52e6301945a9bc30fb8b82d15bb5793f
Ray Otsuka <56874556+ray-o-0509@users.noreply.github.com>
2022-09-07 00:39:11 +0900

    [fix ] fixed secure bug

--- js/common.js
+++ js/common.js
@@ -1,44 +1,18 @@
-
 // auto_scroll ボタンを押して、画面を引数の位置までアニメーションスクロール
 function auto_scroll(num, num2) {
-    var num2 = num2 || 500; 
+    var num2 = num2 || 500;
     $('html,body').animate({
-        scrollTop:$(num).offset().top - 100
-    }, num2, function() {
-    });
+        scrollTop: $(num).offset().top - 100
+    }, num2, function() {});
 }
 
 
 auth_user_type();
-function auth_user_type (){
-    var code_array = localStorage.token.split(',');
-    let right_8_number = parseInt(code_array[0] + code_array[1] + code_array[2] + code_array[3] + code_array[4] + code_array[5] + code_array[6] + code_array[7]);
-    if(right_8_number % 7 === parseInt(code_array[8]) && right_8_number % 9 === parseInt(code_array[9])){
-        if((parseInt(code_array[0] + code_array[1]) % 5) === 1 || (parseInt(code_array[0] + code_array[1]) % 5) === 0){
-            console.log('生徒 / 教員ログイン');
-             $('.hide_projects').removeClass();
-             $('.only_school_projects').removeClass();
-        }else if((parseInt(code_array[0] + code_array[1]) % 5) === 4){
-            if(code_array[2] === '0'){
-                console.log('生徒 / 教員ログイン');
-                $('.hide_projects').removeClass();
-                $('.only_school_projects').removeClass();
-            }else{
-                //一般
-                console.log('一般ログイン');
-                document.documentElement.classList.remove('AltVideo');
-            }    
-        }else if((parseInt(code_array[0] + code_array[1]) % 5) === 2){
-            console.log('受験希望生');
-            
-            $('.hide_projects').removeClass();
-            
-        }else{
-            //一般
-            console.log('一般ログイン');
-            
-        }
-    }else{
-        alert('認証エラー。最初からやり直してください。')
+
+function auth_user_type() {
+    var pw = localStorage.getItem(token);
+    if (pw != '2022274274') {
+        alert('認証エラー。最初からやり直してください。');
+        location.href = 'https://www.hiryusai2022.tk/auth/';
     }
 }
